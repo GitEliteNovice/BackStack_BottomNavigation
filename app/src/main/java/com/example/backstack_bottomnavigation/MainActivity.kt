e66d5460641d0386bb0785204e65d398372a7c53
elgroup <networking@elgroupinternational.com>
2019-07-24 12:36:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/backstack_bottomnavigation/MainActivity.kt
@@ -0,0 +1,141 @@
+package com.example.backstack_bottomnavigation
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
+import com.example.backstack_bottomnavigation.R
+import com.example.backstack_bottomnavigation.fragments.HomeFragment
+import com.example.backstack_bottomnavigation.fragments.MidFragment
+import com.example.backstack_bottomnavigation.fragments.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+import java.util.*
+
+ class MainActivity : AppCompatActivity() {
+  private lateinit var bottomNavigation:BottomNavigationView
+     private var currentTag:String="null"
+     private var currentFragment:Fragment?=null
+      private lateinit var homeFragment:Fragment
+     private lateinit var midFragment:Fragment
+     private lateinit var profileFragment:Fragment
+     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//doWork()
+         init()
+    }
+
+
+
+     fun doWork(){
+         val array= arrayOf(2,3,2,3,5)
+         val hashmap=HashMap<Int,Int>()
+         for (i in 0.. array.size-1){
+             if (hashmap.containsKey(array[i])){
+                 hashmap.put(array[i],(hashmap.get(array[i]) as Int)+1)
+             }else{
+                 hashmap.put(array[i],1)
+             }
+         }
+         publishResult(hashmap)
+
+     }
+
+     private fun publishResult(hashmap: HashMap<Int, Int>) {
+         var iterator=hashmap.entries.iterator()
+         while (iterator.hasNext()){
+             var entry =iterator.next()
+             Log.d("Aryan","${entry.key}--"+entry.value)
+         }
+
+     }
+
+     private fun init() {
+         homeFragment= HomeFragment()
+         midFragment= MidFragment()
+         profileFragment= ProfileFragment()
+
+         bottomNavigation=findViewById(R.id.btm_nav)
+         bottomNavigation.setOnNavigationItemSelectedListener(object :BottomNavigationView.OnNavigationItemSelectedListener{
+             override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                 when(item.itemId){
+                     R.id.action_home->{
+
+
+                         updateFragment(homeFragment,"home")
+                     return true
+                     }
+                     R.id.action_mid->{
+
+                         updateFragment(midFragment,"mid")
+                     return true
+                     }
+                     R.id.action_profile->{
+                         updateFragment(profileFragment,"profile")
+            return true
+                     }
+
+                 }
+return false
+
+             }
+
+         })
+         updateFragment(homeFragment,"home")
+     }
+
+     fun updateFragment(fragment:Fragment, tag:String){
+
+         if (currentTag.equals(tag)){
+             return
+         }
+         var prev_fragment=supportFragmentManager.findFragmentByTag(tag)
+
+         // if fragment is not present in stack
+         if (prev_fragment==null){
+             if (currentFragment==null){
+                 supportFragmentManager.beginTransaction().add(R.id.frag_container,fragment,tag).commit()
+             }else{
+                 supportFragmentManager.beginTransaction().hide(currentFragment!!).add(R.id.frag_container,fragment,tag).commit()
+             }
+
+         }
+       // if fragment is already added
+         else{
+             supportFragmentManager.beginTransaction().hide(currentFragment!!).show(prev_fragment).commit()
+         }
+         FragmentStack.getInstance(3).push(tag)
+         currentFragment=fragment
+         currentTag=tag
+
+     }
+
+     override fun onBackPressed() {
+     var toptag  =  FragmentStack.getInstance(3).pop(currentTag)
+         if (toptag!=null){
+             var topfrag=  supportFragmentManager.findFragmentByTag(toptag)
+             supportFragmentManager.beginTransaction().hide(currentFragment!!).show(topfrag!!).commit()
+         currentTag=toptag
+             currentFragment=topfrag
+             if (currentTag.equals("home")){
+                 bottomNavigation.selectedItemId=R.id.action_home
+             }else if (currentTag.equals("mid")){
+
+                 bottomNavigation.selectedItemId=R.id.action_mid
+             }else if (currentTag.equals("profile")){
+
+                 bottomNavigation.selectedItemId=R.id.action_profile
+             }
+
+
+         }else{
+             super.onBackPressed()
+         }
+
+     }
+}
---
+++ app/src/main/java/com/example/backstack_bottomnavigation/fragments/HomeFragment.kt
@@ -0,0 +1,32 @@
+package com.example.backstack_bottomnavigation.fragments
+
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.backstack_bottomnavigation.R
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment doWork()
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+}
